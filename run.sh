db8b51e9f1051cc11907c2e25f7b0c805ad4a1f3
Rexamm1t <rexamm1t@gmail.com>
2025-08-06 13:23:45 +0300

    Обновить run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,33 @@
 clear
+
+echo "start boldana-project..."
+sleep 3
+
+clear
+
+pkg install zsh -y
+chsh -s zsh
+rm -rf ~/.zshrc
+cp ost/.zshrc ~/
+clear
+echo "1/3 - Zsh настроен"
+sleep 2
+
+rm -rf .termux/font.ttf
+cp ost/font.ttf .termux
+clear 
+echo "1/3 - Zsh настроен"
+echo "2/3 - Шрифты установлен"
+sleep 2
+
+pkg install neofetch -y
+clear
+echo "1/3 - Zsh настроен"
+echo "2/3 - Шрифты установлен"
+echo "3/3 - Окружение настроено"
+sleep 2
+
+clear
+echo "Сейчас вам нужно повторно нажать Enter для завершения..."
+exit
+exit
